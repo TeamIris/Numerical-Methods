582849d0457c053ed47d2acfcfeb3fe8082ac2d0
TeamIris <TeamIris@users.noreply.github.com>
2017-03-22 16:39:28 -0600

    Add Assn 5 Prob 4 file
    This is my cpp file for assn 5 prob 4

---
+++ main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  Assn 5 Prob 4
+//
+//  Created by Eric Alsop on 3/22/17.
+//  Copyright © 2017 Eric Alsop. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+//Function declarations
+//void Simp13(double h, double f0, double f1, double f2);
+//void Simp38(h, f0, f1,f2,f3);
+//void Simp13m(h, n, f);
+//void SimpInt(a,b,n,f);
+//void Print(a,b,n,h,I);
+
+int main() {
+    double a,b,n,h;
+    h=(b-a)/n;
+    vector<double> y;
+    //double I =SimpInt(a,b,n,f);
+    //Print(a,b,n,h,I);
+    
+    
+    return 0;
+}
+
